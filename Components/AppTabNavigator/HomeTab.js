9c2fff017a643c745427a05ffe1a84976c011937
Alexis Araujo <ala1289@gmail.com>
2018-02-06 08:00:00 -0600

    Add Stories Header Component

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -4,9 +4,10 @@ import React, { Component } from 'react'
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  ScrollView
 } from 'react-native'
-import { Container, Content, Icon } from 'native-base'
+import { Container, Content, Icon, Thumbnail } from 'native-base'
 import CardComponent from '../CardComponent'
 
 export default class HomeTab extends Component {
@@ -24,6 +25,33 @@ export default class HomeTab extends Component {
       <Container
         style={styles.container}>
         <Content>
+          <View style={{height: 100}}>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent:'space-between', alignItems: 'center', paddingHorizontal: 7}}>
+              <Text style={{fontWeight: 'bold'}}>Stories</Text>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Icon name='md-play' style={{fontSize: 14}}></Icon>
+                <Text style={{fontWeight: 'bold'}}>Watch All</Text>
+              </View>
+            </View>
+            <View style={{flex: 3}}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                  alignItems:'center',
+                  paddingStart: 5,
+                  paddingEnd: 5
+                }}
+                >
+                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}} source={require('../../assets/StoriesHeaderThumbnails/1.jpg')}/>
+                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}} source={require('../../assets/StoriesHeaderThumbnails/2.jpg')}/>
+                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}} source={require('../../assets/StoriesHeaderThumbnails/3.jpg')}/>
+                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}} source={require('../../assets/StoriesHeaderThumbnails/4.jpg')}/>
+                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}} source={require('../../assets/StoriesHeaderThumbnails/6.jpg')}/>
+                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}} source={require('../../assets/StoriesHeaderThumbnails/7.jpg')}/>
+              </ScrollView>
+            </View>
+          </View>
           <CardComponent imageSource='1' likes='101'/>
           <CardComponent imageSource='2' likes='201'/>
           <CardComponent imageSource='3' likes='301'/>
